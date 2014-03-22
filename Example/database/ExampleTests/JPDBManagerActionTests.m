a3494fa068d48f19148e6148152190c189b0c5a9
Paulo Oliveira <eu@pauloliveira.net>
2014-03-22 19:45:47 -0300

    Finishing tests

--- Example/database/ExampleTests/JPDBManagerActionTests.m
+++ Example/database/ExampleTests/JPDBManagerActionTests.m
@@ -68,12 +68,6 @@ describe(@"ManagerAction", ^{
             [[result should] equal:action];
             [[action.variablesListAndValues should] equal:emptyDictionary];
 
-            result = [action applyFetchReplaceWithVariables:@"value", @"key", nil];
-            [[result should] equal:action];
-            [result.variablesListAndValues shouldNotBeNil];
-            NSString *assigned = action.variablesListAndValues[@"key"];
-            [[assigned should] equal:@"value"];
-
             NSPredicate *predicate = [NSPredicate new];
             result = [action applyPredicate:predicate];
             [[result should] equal:action];
@@ -277,8 +271,300 @@ describe(@"ManagerAction", ^{
         });
 
 
+
+
+        it(@"Query specified Entity using one specified Fetch Template name, replacing data with dictionary", ^{
+            NSDictionary *replaceWith = @{@"anKey": @"anValue"};
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[1] should] equal:__fetchTemplate];
+                   [[params[2] should] equal:replaceWith];
+                   return nil;
+               }
+            ];
+            [action queryEntity:__entityName withFetchTemplate:__fetchTemplate replaceFetchWithDictionary:replaceWith];
+        });
+
+
+
+
+
+        it(@"Query specified Entity using one specified Fetch Template name, replacing data with dictionary"
+           @"ordering by key", ^{
+            NSDictionary *replaceWith = @{@"anKey": @"anValue"};
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[1] should] equal:__fetchTemplate];
+                   [[params[2] should] equal:replaceWith];
+                   [params[3] each:^( NSSortDescriptor *item ) {
+                       [[item.key should] equal:@"_key_"];
+                   }];
+                   return nil;
+               }
+            ];
+            [action queryEntity:__entityName withFetchTemplate:__fetchTemplate
+                                    replaceFetchWithDictionary:replaceWith
+                                                  orderWithKey:@"_key_"];
+        });
+
+
+
+
+        it(@"Query specified Entity using one specified Fetch Template name, replacing data with dictionary"
+                @"ordering by keys", ^{
+            NSDictionary *replaceWith = @{@"anKey": @"anValue"};
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[1] should] equal:__fetchTemplate];
+                   [[params[2] should] equal:replaceWith];
+                   [params[3] eachWithIndex:^( NSSortDescriptor *item, NSUInteger index) {
+                       if ( index == 0 ) [[item.key should] equal:@"keyA"];
+                       if ( index == 1 ) [[item.key should] equal:@"keyB"];
+                   }];
+                   return nil;
+               }
+            ];
+            [action queryEntity:__entityName withFetchTemplate:__fetchTemplate
+                                    replaceFetchWithDictionary:replaceWith
+                                                 orderWithKeys:@"keyA", @"keyB", nil];
+        });
+
+
+
+
+        it(@"Query specified Entity using one specified Fetch Template name, replacing data with dictionary"
+                @"ordering by array of sort descriptors", ^{
+            NSDictionary *replaceWith = @{@"anKey": @"anValue"};
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[1] should] equal:__fetchTemplate];
+                   [[params[2] should] equal:replaceWith];
+                   [params[3] eachWithIndex:^( NSSortDescriptor *item, NSUInteger index) {
+                       if ( index == 0 ) [[item.key should] equal:@"keyA"];
+                       if ( index == 1 ) [[item.key should] equal:@"keyB"];
+                   }];
+                   return nil;
+               }
+            ];
+            [action queryEntity:__entityName withFetchTemplate:__fetchTemplate
+                         replaceFetchWithDictionary:replaceWith
+                             arrayOfSortDescriptors:
+                                     @[
+                                             [NSSortDescriptor sortDescriptorWithKey:@"keyA" ascending:YES],
+                                             [NSSortDescriptor sortDescriptorWithKey:@"keyB" ascending:YES]
+                                     ]
+            ];
+        });
+
+
+
+
+        it(@"Should query specified Entity using one custom NSPredicate", ^{
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[4] should] beKindOfClass:[NSPredicate class]];
+                   return nil;
+               }
+
+            ];
+            [action queryEntity:__entityName withPredicate:[NSPredicate  new]];
+        });
+
+
+
+
+        it(@"Should query specified Entity using one custom NSPredicate and specified key", ^{
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[4] should] beKindOfClass:[NSPredicate class]];
+                   [params[3] each:^( NSSortDescriptor *item ) {
+                       [[item.key should] equal:@"_key_"];
+                   }];
+                   return nil;
+               }
+
+            ];
+            [action queryEntity:__entityName withPredicate:[NSPredicate  new] orderWithKey:@"_key_"];
+        });
+
+
+
+
+        it(@"Should query specified Entity using one custom NSPredicate and specified keys", ^{
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[4] should] beKindOfClass:[NSPredicate class]];
+                   [params[3] eachWithIndex:^( NSSortDescriptor *item, NSUInteger index) {
+                       if ( index == 0 ) [[item.key should] equal:@"keyA"];
+                       if ( index == 1 ) [[item.key should] equal:@"keyB"];
+                   }];
+                   return nil;
+               }
+
+            ];
+            [action queryEntity:__entityName withPredicate:[NSPredicate  new] orderWithKeys:@"keyA", @"keyB", nil];
+        });
+
+
+
+
+        it(@"Should query specified Entity using one custom NSPredicate and sort descriptors", ^{
+            [action stub:finalMethod
+
+               withBlock:^id(NSArray *params) {
+                   [[params[0] should] equal:__entityName];
+                   [[params[4] should] beKindOfClass:[NSPredicate class]];
+                   [params[3] eachWithIndex:^( NSSortDescriptor *item, NSUInteger index) {
+                       if ( index == 0 ) [[item.key should] equal:@"keyA"];
+                       if ( index == 1 ) [[item.key should] equal:@"keyB"];
+                   }];
+                   return nil;
+               }
+
+            ];
+            [action queryEntity:__entityName withPredicate:[NSPredicate  new]
+                         arrayOfSortDescriptors:
+                                    @[
+                                            [NSSortDescriptor sortDescriptorWithKey:@"keyA" ascending:YES],
+                                            [NSSortDescriptor sortDescriptorWithKey:@"keyB" ascending:YES]
+                                    ]
+            ];
+        });
+    });
+
+    ////////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+    context(@"Query Call Manager", ^{
+
+        it(@"Final method should call manager", ^{
+            #pragma clang diagnostic push
+            #pragma clang diagnostic ignored "-Wundeclared-selector"
+
+            // Stub the manager to receive internal call.
+            [manager stub:@selector(performDatabaseAction:) withArguments:action];
+            [[manager should] receive:@selector(performDatabaseAction:) withArguments:action];
+
+            [action queryEntity:__entityName withFetchTemplate:__fetchTemplate
+                         replaceFetchWithDictionary:[NSDictionary new]
+                             arrayOfSortDescriptors:
+                                     @[
+                                             [NSSortDescriptor sortDescriptorWithKey:@"keyA" ascending:YES],
+                                             [NSSortDescriptor sortDescriptorWithKey:@"keyB" ascending:YES]
+                                     ]
+                                    customPredicate:[NSPredicate new]
+            ];
+
+            #pragma clang diagnostic pop
+        });
+
+    });
+
+    ////////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+    context(@"Remove Data", ^{
+
+        // All remove methods concatenate to call one final method, we'll stub and expect data from him
+        // in all query tests.
+        __block SEL finalMethod = @selector(deleteRecord:andCommit:);
+
+        // Mock data object to delete.
+        __block id dataObject = [KWMock mockForClass:[NSManagedObject class]];
+
+        beforeEach(^{
+            // Stub the final method.
+            [action stub:finalMethod];
+        });
+
+        it(@"Should delete all Records from specified entity", ^{
+            // Stub internal query to return our data object.
+            [action stub:@selector(queryEntity:withFetchTemplate:) andReturn:@[dataObject]];
+
+            // Delete all.
+            [[action should] receive:finalMethod withArguments:dataObject, [KWValue valueWithBool:NO]];
+            [action deleteAllRecordsFromEntity:@"_entity_"];
+        });
+
+
+
+
+
+        it(@"Shoul delete all records queried by the specified Fetch Template", ^{
+            // Stub internal query to return our data object.
+            [action stub:@selector(queryEntity:withFetchTemplate:) andReturn:@[dataObject]
+                                                               withArguments:@"_entity_", @"_template"];
+
+            // Delete all.
+            [[action should] receive:finalMethod withArguments:dataObject, [KWValue valueWithBool:NO]];
+            [action deleteRecordsFromEntity:@"_entity_" withFetchTemplate:@"_template"];
+        });
+
+        
+        
+        
+        
+        it(@"Should elete an record of database", ^{
+            [[action should] receive:finalMethod withArguments:dataObject, [KWValue valueWithBool:NO]];
+            [action deleteRecord:dataObject];
+        });
+
     });
     
+    ////////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+    
+    context(@"Delete Call Manager", ^{
+
+        it(@"Should delete an record of database, final call", ^{
+            id dataObject = [KWMock mockForClass:[NSManagedObject class]];
+
+            // Stub the manager to receive internal calls.
+            [manager stub:@selector(deleteRecord:)];
+            [manager stub:@selector(commit)];
+
+            [[manager should] receive:@selector(deleteRecord:) withArguments:dataObject];
+            [[manager should] receive:@selector(commit)];
+
+            [action deleteRecord:dataObject andCommit:YES];
+        });
+
+    });
+
+    ////////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// ///////// /////////
+
+    context(@"Create Data", ^{
+
+        // Mock data object to create
+        __block id dataObject = [KWMock mockForClass:[NSManagedObject class]];
+
+        it(@"Should create and return a new empty Record for specified Entity, final call", ^{
+
+            // Stub the manager to receive internal calls.
+            [manager stub:@selector(createNewRecordForEntity:) andReturn:dataObject];
+            [[manager should] receive:@selector(createNewRecordForEntity:) withArguments:@"_entity_"];
+
+
+            id result = [action createNewRecordForEntity:@"_entity_"];
+            [[action.entity should] equal:@"_entity_"];
+
+            [result shouldNotBeNil];
+            [[result should] equal:dataObject];
+        });
+        
+    });
+
 });
 
 SPEC_END
