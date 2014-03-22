4fdd30ff0c44c5e6249202927d08a56a82d1006c
Paulo Oliveira <eu@pauloliveira.net>
2014-03-22 12:43:18 -0300

    Creating Database Example Project

---
+++ Example/database/Example/JPViewController.m
@@ -0,0 +1,29 @@
+//
+//  JPViewController.m
+//  Example
+//
+//  Created by Paulo Oliveira on 3/22/14.
+//  Copyright (c) 2014 seqoy.org. All rights reserved.
+//
+
+#import "JPViewController.h"
+
+@interface JPViewController ()
+
+@end
+
+@implementation JPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
